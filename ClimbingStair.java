72c6b65ba63175566f40035df7bd56cb626ea8fc
Raza_Web Development <razabtech27@gmail.com>
2025-05-08 15:51:00 +0530

    new change

---
+++ ClimbingStair.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ClimbingStair {
+    // climbing stair based fabonacci logic of dp
+  public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+    
+  }    
+}
--- Revisitng_Fabo.java
+++ Revisitng_Fabo.java
@@ -18,8 +18,21 @@ public class Revisitng_Fabo {
     Scanner sc = new Scanner(System.in) ;
     System.out.print("Enter a number n to get nth fabonacci number : ");
     int n = sc.nextInt();
-    int fib[] = new int[n+1];// at this java automatically store 0 to each index
-    int res = fibo(n,fib);
+    //int fib[] = new int[n+1];// at this java automatically store 0 to each index
+    // int res = fibo(n,fib);
+    int res = fibo2(n);
     System.out.println(n+"th fabonacii seris : "+res);
   }    
+//let's do fibo(n) by tabulation way of DP i.e way-2
+ static int fibo2(int n){// O(n)
+    int dp[] = new int[n+1];
+// i) initailization
+    dp[1] = 1;
+// ii) assign meaning to each of the dp index in your mind and i.e dp[i]-- ith fibonacci number means = sum of ith num is equal to (i-1)th + (i-2)th numbers
+// iii) filling from small to large
+ for(int i = 2; i<=n; i++){
+   dp[i] = dp[i-1] + dp[i-2];
+  }
+  return dp[n];// our ans
+ }
 }
